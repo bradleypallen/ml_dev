d08d427d8690deb1f6fd3642442ce4bafdabf174
Bradley P. Allen <bradley.p.allen@gmail.com>
2014-10-02 20:17:59 -0700

    Added fabfile.py and data files.
    Data comes from several datasets in the UCI ML Repository, edited
    slightly in several cases to get the data files to fit CSV assumptions
    that the fabfile assumes.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 The purpose of this image is to support the rapid development of machine learning applications using Vowpal Wabbit, a terascale online learning engine that can be used to train a variety of linear models for prediction, classification, LDA. etc. Perf is included to support the evaluation of such models by calculating different performance metrics such as ROC, F-measure, accuracy, etc.
 
-This image is based on Ubuntu:latest. Source for vw and perf installed to /usr/local/src. Additional tools installed are emacs, g++, git, wget, curl and python. Pip, virtualenv and fabric installed for using python to orchestrate data ETL and prototype machine learning pipelines.
+This image is based on Ubuntu:latest. Source for vw and perf installed to /usr/local/src. Additional tools installed are emacs, g++, git, wget, curl and python. Pip, virtualenv and fabric installed for using python to orchestrate data ETL and prototype machine learning pipelines. fabfile.py provides a simple workflow for generating and evaluating models trained from data provided in CSV format.
 
 ## License
 
---
+++ fabfile.py
@@ -0,0 +1,63 @@
+from fabric.api import task, local
+import csv, random
+
+def record_to_vw(record, feature_fields, target_field):
+	"""
+	Convert a Python dictionary into a string in Vowpal Wabbit data format.
+	"""
+	vw_record = "%s |" % record[target_field]
+	for f in feature_fields:
+		vw_record += " %s:%s" % (f.replace(' ', '_'), record[f])
+	return vw_record + "\n"	
+
+@task
+def prepare_data(data='data.csv', target_field='class', test_holdout=0.1, train='train.dat', validate='validate.dat'):
+	"""
+	Transform CSV data file into train/validate files in Vowpal Wabbit data format.
+	"""
+	records = list(csv.DictReader(open(data)))
+	feature_fields = records[0].keys()
+	feature_fields.remove(target_field)
+	random.shuffle(records)
+	split_idx = int(float(len(records))*test_holdout)
+  	with open(train, "wb") as outfile:
+		for record in records[split_idx:]:
+			outfile.write(record_to_vw(record, feature_fields, target_field))
+  	with open(validate, "wb") as outfile:
+		for record in records[:split_idx]:
+			outfile.write(record_to_vw(record, feature_fields, target_field))
+
+@task
+def train(data='train.dat', model='model.vw', predictions='predict.dat', passes=0):
+	"""
+	Create a Vowpal Wabbit model by training it on a training data set.
+	"""
+	if passes > 0:
+		local("vw -d %s -f %s -p %s" % (data, model, predictions))
+	else:
+		local("vw -d %s -c --passes=%s -f %s -p %s" % (data, passes, model, predictions))
+
+@task
+def varinfo(data='train.dat', passes=0):
+	"""
+	Display the features in a Vowpal Wabbit model.
+	"""
+	if passes > 0:
+		local("vw-varinfo %s" % data)
+	else:
+		local("vw-varinfo %s -c --passes=%s" % (data, passes))
+
+@task
+def validate(data='validate.dat', model='model.vw', predictions='predict.dat'):
+	"""
+	Validate a Vowpal Wabbit model by testing it on a validation data set.
+	"""
+	local("vw -d %s -t -f %s -p %s" % (data, model, predictions))
+
+@task
+def performance(predictions='predict.dat'):
+	"""
+	Display the performance of a Vowpal Wabbit model.
+	"""
+	# Load the index mappings schema
+	local("pref < %s" % predictions)
