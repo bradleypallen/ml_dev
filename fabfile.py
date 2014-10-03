141b13e3ead290f91c875b215a671aca585738c5
Bradley P. Allen <bradley.p.allen@gmail.com>
2014-10-03 00:48:17 -0700

    Fixes to fabfile.py
    Added code to handle logistic regression as a default model type. Fixed
    a problem with the use of the passes parameter. Made the performance
    task dump all perf statistics.

--- fabfile.py
+++ fabfile.py
@@ -11,7 +11,7 @@ def record_to_vw(record, feature_fields, target_field):
 	return vw_record + "\n"	
 
 @task
-def prepare_data(data='data.csv', target_field='class', test_holdout=0.1, train='train.dat', validate='validate.dat'):
+def prepare_data(data='data.csv', target_field='class', validate_holdout=0.1, train='train.dat', validate='validate.dat'):
 	"""
 	Transform CSV data file into train/validate files in Vowpal Wabbit data format.
 	"""
@@ -19,7 +19,7 @@ def prepare_data(data='data.csv', target_field='class', test_holdout=0.1, train=
 	feature_fields = records[0].keys()
 	feature_fields.remove(target_field)
 	random.shuffle(records)
-	split_idx = int(float(len(records))*test_holdout)
+	split_idx = int(float(len(records))*validate_holdout)
   	with open(train, "wb") as outfile:
 		for record in records[split_idx:]:
 			outfile.write(record_to_vw(record, feature_fields, target_field))
@@ -28,37 +28,43 @@ def prepare_data(data='data.csv', target_field='class', test_holdout=0.1, train=
 			outfile.write(record_to_vw(record, feature_fields, target_field))
 
 @task
-def train(data='train.dat', model='model.vw', predictions='predict.dat', passes=0):
+def train(data='train.dat', model='model.vw', predictions='predict.dat', passes=1, logistic=True):
 	"""
 	Create a Vowpal Wabbit model by training it on a training data set.
 	"""
-	if passes == 0:
-		local("vw -d %s -f %s -p %s" % (data, model, predictions))
+	if logistic:
+		local("vw -d %s -c --passes=%s -f %s -p %s --loss-function=logistic --link=logistic" % (data, passes, model, predictions))
 	else:
 		local("vw -d %s -c --passes=%s -f %s -p %s" % (data, passes, model, predictions))
 
 @task
-def varinfo(data='train.dat', passes=0):
+def varinfo(data='train.dat', passes=1, logistic=True):
 	"""
 	Display the features in a Vowpal Wabbit model.
 	"""
-	if passes == 0:
-		local("/usr/local/src/vowpal_wabbit/utl/vw-varinfo %s" % data)
+	if logistic:
+		local("/usr/local/src/vowpal_wabbit/utl/vw-varinfo -c --passes=%s  --loss-function=logistic %s" % (passes, data))
 	else:
 		local("/usr/local/src/vowpal_wabbit/utl/vw-varinfo -c --passes=%s %s" % (passes, data))
 
 @task
-def validate(data='validate.dat', model='model.vw', predictions='predict.dat'):
+def validate(data='validate.dat', model='model.vw', predictions='predict.dat', logistic=True):
 	"""
 	Validate a Vowpal Wabbit model by testing it on a validation data set.
 	"""
-	local("vw -d %s -i %s -p %s" % (data, model, predictions))
+	if logistic:
+		local("vw -d %s -i %s -r %s --loss_function=logistic" % (data, model, predictions))
+	else:
+		local("vw -d %s -i %s -r %s" % (data, model, predictions))
 
 @task
-def performance(data='validate.dat', predictions='predict.dat', metric='ROC'):
+def performance(data='validate.dat', predictions='predict.dat', metric=None):
 	"""
 	Display the performance of a Vowpal Wabbit model.
 	"""
 	local("cut -d ' ' -f 1 %s | sed -e 's/^-1/0/' > gold.dat" % data)
 	local("/usr/local/src/vowpal_wabbit/utl/logistic -0 %s > probabilities.dat" % predictions)
-	local("perf -%s -files gold.dat probabilities.dat" % metric) 
+	if metric:
+		local("perf -%s -files gold.dat probabilities.dat" % metric)
+	else:
+		local("perf -files gold.dat probabilities.dat")
